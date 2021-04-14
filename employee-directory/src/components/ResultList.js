f77ae813a7b4c7c90a4db6efa21c20fe9819cdb7
JaredWeav <jared.weaver2@gmail.com>
2021-04-14 15:39:19 -0400

    added phone number and email to table

--- employee-directory/src/components/ResultList.js
+++ employee-directory/src/components/ResultList.js
@@ -1,41 +1,50 @@
 import React from 'react';
 
-const  ResultList = (props) => {
+const ResultList = (props) => {
   return (
     <table className="table table-dark">
       <thead>
         <tr>
-          <th scope="col">#</th>
-          <th scope="col">Picture</th>
-          <th scope="col">First</th>
-          <th scope="col" onClick={props.handleSort} >Last</th>
-          <th scope="col">Location</th>
+          <th scope="col"></th>
+          <th scope="col">First Name</th>
+          <th scope="col" onClick={props.handleSort}>
+            Last Name
+          </th>
+          <th scope="col">Email</th>
+          <th scope="col">Phone Number</th>
         </tr>
       </thead>
       <tbody>
-        {props.employees.length > 0 ? props.employees.map(employee => {
-          console.log(employee)
-          return (
-            <tr>
-          <th scope="row">1</th>
-          <td><img src={employee.picture.thumbnail} alt=""/></td>
-          <td>{employee.name.first}</td>
-          <td>{employee.name.last}</td>
-          <td>{employee.location.city}, {employee.location.state}</td>
-        </tr>
-          )
-        }) :  <tr>
-          <th scope="row">1</th>
-          <td><img src="" alt=""/></td>
-          <td>First Name</td>
-          <td>Last Name</td>
-          <td>Location</td>
-        </tr>}
-        
+        {props.employees.length > 0 ? (
+          props.employees.map((employee) => {
+            console.log(employee);
+            return (
+              <tr>
+                <td>
+                  <img src={employee.picture.large} alt="Photo of employee" />
+                </td>
+                <td>{employee.name.first}</td>
+                <td>{employee.name.last}</td>
+                <td>{employee.email}</td>
+                <td>{employee.cell}</td>
+              </tr>
+            );
+          })
+        ) : (
+          <tr>
+            <td>
+              <img src="" alt="" />
+            </td>
+            <td>First Name</td>
+            <td>Last Name</td>
+            <td>Email</td>
+            <td>Phone Number</td>
+          </tr>
+        )}
       </tbody>
     </table>
   );
-}
+};
 
 export default ResultList;
 //     <ul className="list-group">
