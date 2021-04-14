4cac01b5931e425635bf021ec0887c4b526b9d59
JaredWeav <jared.weaver2@gmail.com>
2021-04-14 19:33:24 -0400

    formatting

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const BASEURL = "https://randomuser.me/api/?results=20&nat=us";
-// const APIKEY = "";
 
-// Export an object with a "search" method that searches the Giphy API for the passed query
 export default {
   employees: function(query) {
     return axios.get(BASEURL + query);
